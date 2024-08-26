f50522124ed639fb8c7cc32e6cf915a8fd963f12
Eric Garza Gonzalez <eric.garzagnzlz@uanl.edu.mx>
2024-08-25 22:21:23 -0600

    Make dwmblocks volume part listen to signal 10

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Vol: ", "pamixer --get-volume | awk '{print $0 \"%\"}'",	30,		0},
+	{"Vol: ", "pamixer --get-volume | awk '{print $0 \"%\"}'",	30,		10},
 	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"CPU: ", "~/.local/bin/dwmscripts/build/dwmscripts cpu",	5,		0},
 	{"", "date '+%F %H:%M'",					5,		0},
