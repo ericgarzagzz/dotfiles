f8e25142b3b8acf7667af1f41f91c109af2bd3fa
Eric Garza Gonzalez <eric.garzagnzlz@uanl.edu.mx>
2024-08-31 20:06:58 -0600

    Dwmblocks update

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Vol: ", "pamixer --get-volume | awk '{print $0 \"%\"}'",	30,		10},
-	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"CPU: ", "~/.local/bin/dwmscripts/build/dwmscripts cpu",	5,		0},
-	{"", "date '+%F %H:%M'",					5,		0},
+	{"    ", "pamixer --get-volume | awk '{print $0 \"%\"}'",	30,		10},
+	{"    ", "xkblayout-state print \"%s\"",			5,		0},
+	{"    ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	0},
+	{"    ", "~/.local/bin/dwmscripts/build/dwmscripts cpu",	5,		0},
+	{"    ", "date '+%F %H:%M'",					5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
